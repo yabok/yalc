99637b3fe5d8c9ef3a27fbac50d54d031e063dff
HalosGhost <halosghost@archlinux.info>
2015-01-19 10:09:47 -0600

    Some style things that make me feel happier

--- src/yalc.c
+++ src/yalc.c
@@ -45,11 +45,11 @@ main (int32_t argc, char * argv []) {
 }
 
 void
-colorized_print (FILE *stream, enum plane p) {
+colorized_print (FILE * stream, enum plane p) {
 	wchar_t line [BUFFER_SIZE];
 
-	for (uint64_t i = 0; fgetws(line, BUFFER_SIZE, stream); i ++) {
-		for (uint64_t j = 0; j < wcslen(line); j ++) {
+	for ( uint64_t i = 0; fgetws(line, BUFFER_SIZE, stream); i ++ ) {
+		for ( uint64_t j = 0; j < wcslen(line); j ++ ) {
 			printf("\x1b[%d8;5;%sm%lc", p, colors[(j/3+i)%22], line[j]);
 		}
 	}
