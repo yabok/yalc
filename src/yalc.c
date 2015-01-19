960025885204c5f0ce9cdbf729742a97c6fd15b8
HalosGhost <halosghost@archlinux.info>
2015-01-19 07:28:50 -0600

    Argument parsing \o/

--- src/yalc.c
+++ src/yalc.c
@@ -9,10 +9,34 @@
 
 #include "yalc.h"
 
+struct args {
+	enum plane p;
+};
+
+const char * argp_program_version = "yalc 0.0.1";
+const char * argp_program_bug_address = "https://github.com/yabok/yalc/issues";
+static char * doc = "yalc -- a minimalistic lolcat clone in C";
+
+static error_t
+parse_opt (int32_t, char *, struct argp_state *);
+
 int32_t
-main (void) {
+main (int32_t argc, char * argv []) {
 	setlocale(LC_ALL, "");
-	colorized_print(stdin, FOREGROUND);
+
+	struct argp_option os [] = {
+		{ 0,            0,   0, 0, "Options:",                      -1 },
+		{ "foreground", 'f', 0, 0, "Colorize foreground (default)", 0  },
+		{ "background", 'b', 0, 0, "Colorize background",           0  },
+		{ 0,            0,   0, 0, 0,                               0  }
+    };
+
+	struct argp argp = { os, parse_opt, "", doc, NULL, NULL, 0 };
+	struct args args = { FOREGROUND };
+
+	argp_parse(&argp, argc, argv, 0, 0, &args);
+
+	colorized_print(stdin, args.p);
 	return 0;
 }
 
@@ -27,5 +51,20 @@ colorized_print (FILE *stream, enum plane p) {
 	}
 
 	printf("\x1b[0m");
+}
+
+static error_t
+parse_opt (int32_t key, char * arg, struct argp_state * state) {
+	struct args * args = state->input;
+	switch ( key ) {
+		case 'f':
+			args->p = FOREGROUND; break;
+
+		case 'b':
+			args->p = BACKGROUND; break;
+
+		default:
+			return ARGP_ERR_UNKNOWN;
+	} return 0;
 }
 
--- src/yalc.h
+++ src/yalc.h
@@ -14,6 +14,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <wchar.h>
+#include <argp.h>
 
 #define BUFFER_SIZE 5000
 
